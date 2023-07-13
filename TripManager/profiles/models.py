55f7d0e30041cc1b167665bd93f50d4798d3ec34
Prakash Lawagun <prakashlawagun11@gmail.com>
2023-07-13 08:34:06 +0545

    Add requirements.txt

--- TripManager/profiles/models.py
+++ TripManager/profiles/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from account.models import User
-from django.core.validators import RegexValidator
 
-phone_regex = RegexValidator(
-    regex=r'^\+?977?\d{11}$',
-    message="Phone number must be entered in the format: '+977999999999.'"
-)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,7 +8,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
-    phone = models.CharField(max_length=14, validators=[phone_regex])
+    phone = models.CharField(max_length=10)
 
     def __str__(self):
         return self.user.username
